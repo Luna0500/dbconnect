498c9adaac80b2f800e8b126c050dca4bfd9c324
Ryan Lunas <122395549+Luna0500@users.noreply.github.com>
2023-07-09 08:30:31 -0600

    Create dbtest.js

---
+++ pages/dbtest.js
@@ -0,0 +1,38 @@
+import clientPromise from "../lib/mongodb";
+
+export default function Users({ users }) {
+    return (
+        <div>
+            <h1>Testing User List</h1>
+            <ul>
+                {users.map((user) => (
+                    <li>
+                        <h2>{user.name}</h2>
+                        <h3>{user.age}</h3>
+                        <p>{user.email}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export async function getServerSideProps() {
+    try {
+        const client = await clientPromise;
+        const db = client.db("test_db");
+
+        const users = await db
+            .collection("users")
+            .find({})
+            .sort({ name: -1 })
+            .limit(20)
+            .toArray();
+
+        return {
+            props: { users: JSON.parse(JSON.stringify(users)) },
+        };
+    } catch (e) {
+        console.error(e);
+    }
+}
