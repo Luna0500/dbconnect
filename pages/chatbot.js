1be56cb01ad18a6010c8c5b43289038d270f4504
Ryan Lunas <122395549+Luna0500@users.noreply.github.com>
2023-07-09 11:01:49 -0600

    fix

---
+++ pages/chatbot.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+
+const openai = new OpenAIApi(configuration);
+const messages = [
+  { role: 'user', content: 'write a whimsical paragraph about software development' }
+];
+
+const doIt = async () => {
+  try {
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages
+    });
+    console.log(completion.data);
+    console.log(completion.data.choices[0].message);
+    return {
+      props: {chattext: completion.data},
+    };
+
+  } catch (error) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+    }
+  }
+}
+
+doIt();
+
+export default function App({ chattext }) {
+  return (
+      <p>{chattext}</p>
+  );
+}
