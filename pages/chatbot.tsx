a7a85da0cca788d8564e370fc10927c3dbf0454b
Ryan Lunas <122395549+Luna0500@users.noreply.github.com>
2023-07-09 10:15:19 -0600

    fix

--- pages/chatbot.tsx
+++ pages/chatbot.tsx
@@ -1,27 +1,53 @@
-import { Layout, Text, Page } from '@vercel/examples-ui'
-import { Chat } from '../components/Chat'
+const { Configuration, OpenAIApi } = require("openai");
+const readlineSync = require("readline-sync");
+require("dotenv").config();
 
-function Home() {
-    return (
-        <Page className="flex flex-col gap-12">
-            <section className="flex flex-col gap-6">
-                <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
-                <Text className="text-zinc-600">
-                    In this example, a simple chat bot is implemented using Next.js, API
-                    Routes, and OpenAI API.
-                </Text>
-            </section>
+(async () => {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
 
-            <section className="flex flex-col gap-3">
-                <Text variant="h2">AI Chat Bot:</Text>
-                <div className="lg:w-2/3">
-                    <Chat />
-                </div>
-            </section>
-        </Page>
-    )
-}
+  const history = [];
 
-Home.Layout = Layout
+  while (true) {
+    const user_input = readlineSync.question("Your input: ");
 
-export default Home
+    const messages = [];
+    for (const [input_text, completion_text] of history) {
+      messages.push({ role: "user", content: input_text });
+      messages.push({ role: "assistant", content: completion_text });
+    }
+
+    messages.push({ role: "user", content: user_input });
+
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+
+      const completion_text = completion.data.choices[0].message.content;
+      console.log(completion_text);
+
+      history.push([user_input, completion_text]);
+
+      const user_input_again = readlineSync.question(
+        "\nWould you like to continue the conversation? (Y/N)"
+      );
+      if (user_input_again.toUpperCase() === "N") {
+        return;
+      } else if (user_input_again.toUpperCase() !== "Y") {
+        console.log("Invalid input. Please enter 'Y' or 'N'.");
+        return;
+      }
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status);
+        console.log(error.response.data);
+      } else {
+        console.log(error.message);
+      }
+    }
+  }
+})();
