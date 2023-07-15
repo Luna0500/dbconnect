ec7bf1cb2261843a9e391bd1f77ec88836054bad
Ryan Lunas <122395549+Luna0500@users.noreply.github.com>
2023-07-14 23:59:17 -0600

    update

---
+++ pages/form.js
@@ -0,0 +1,27 @@
+import clientPromise from "../lib/mongodb";
+export default function Form() {
+    return (
+        <div>
+        <form onsubmit="submitForm()">
+            <label htmlFor="first">First Name</label>
+            <input type="text" id="first" name="first" required />
+
+            <label htmlFor="last">Last Name</label>
+            <input type="text" id="last" name="last" required />
+
+            <button type="submit">Submit</button>
+        </form>
+        </div>
+    )
+}
+
+export async function submitForm() {
+    const client = await clientPromise;
+    const db = client.db("test_db");
+    const collec = db.collection("name");
+
+    const formData = {
+        "first": first
+    }
+    const result = await collec.insertOne(formData);
+}
